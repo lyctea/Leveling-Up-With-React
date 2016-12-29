f5b38e9a287e0ccf4e1fb971d7f715f45bb80d2f
lyctea <396136337@qq.com>
2016-12-29 09:36:23 +0800

    静态HTML转React组件

--- 1.3.js
+++ 1.3.js
@@ -21,3 +21,32 @@
         </main>
     </div>
 </div>
+
+/*写好静态HTML结构之后,转换成React component*/
+/*For components that aren't parent components,
+this.props.children will be null
+当组件没有父组件的时候this.props.children为空*/
+var SearchLayout = React.createClass({
+    render: function () {
+        return (
+            <div className="search">
+                <header className="search-header"></header>
+                <div className="results">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+});
+
+var UserList = React.createClass({
+    render: function () {
+        return (
+            <ul className="user-list">
+                <li>Dan</li>
+                <li>Ryan</li>
+                <li>Michael</li>
+            </ul>
+        );
+    }
+});
