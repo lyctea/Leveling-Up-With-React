30fccb6b8949fea3fd5a16df092dedbac7b5c1a8
lyctea <396136337@qq.com>
2016-12-29 20:42:18 +0800

    Iterations
    子组件迭代, 需要注意的是每个子组件都必须有唯一的key

--- 2.2.js
+++ 2.2.js
@@ -2,14 +2,15 @@ var UserList = React.createClass({
     render: function () {
         return (
             <ul className="user-list">
-                {this.props.users.map(function (user) {
-                    return (
-                        <li key={user.id}>
-                            <Link to="{'/users' + user.id}">{user.name}</Link>
-                        </li>
-                    )
-                })}
+                {this.props.users.map(this.createListItem)}
             </ul>
         )
+    },
+    createListItem: function (user) {
+        return (
+            <li key={user.id}>
+                <Link to="{'/users' + user.id}">{user.name}</Link>
+            </li>
+        );
     }
 });
