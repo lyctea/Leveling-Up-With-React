231772e28a526d0373d59fab6f56bdab35aabdc8
lyctea <396136337@qq.com>
2016-12-29 10:35:03 +0800

    Link标签及作用

--- 1.4.js
+++ 1.4.js
@@ -10,3 +10,23 @@
     <Route path="inventory" component={Productinventory} />
     <Route path="orders" component={Productorders} />
 </Route>
+
+
+
+/*<Link>标签相当于<a>标签
+<ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/users">Users</Link></li>
+    <li><Link to="/widgets">Widgets</Link></li>
+</ul>
+
+
+ <Link to="/users" className="users">会被转换成
+ <a href="/users" class="users">
+
+    如果需要一个无路由路径的链接,可以使用a标签
+
+    <Link>知道自己被激活
+ <Link to="/users" activeClassName="active">Users</Link>
+ 当用户浏览到/users时, Link标签将加载active样式
+*/
